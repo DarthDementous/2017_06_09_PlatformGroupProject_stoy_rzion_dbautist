522dda72774675c29db97b1d92ec8be9e0a0b6c9
WolvenStark <wolvenstark@gmail.com>
2017-09-06 16:48:28 +1000

    ~ Re-upload Assets folder.

---
+++ UnityCrossPlatformProject_stoy_rzion_dbautist/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour {
+
+	public float initialSpeed;
+	public float screenRange;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x, move_dist, move_speed;
+		 x = Input.GetAxis ("Horizontal");
+
+		move_speed = ((initialSpeed + Time.deltaTime));
+		move_dist = (Time.deltaTime * move_speed) * x;
+
+		// move the player respectivley
+		transform.Translate (move_dist, 0, 0);
+
+		// restrict the player to between the screen bounds.
+		transform.position = new Vector3 (Mathf.Clamp (transform.position.x, -screenRange, screenRange), transform.position.y, 0);
+
+
+	}
+}
+	
---
+++ UnityCrossPlatformProject_stoy_rzion_dbautist/Assets/Scripts/enemyHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyHandler : MonoBehaviour {
+
+	public float moveSpeed;			
+	public float xTargetPos;		/*Position to move to on the x axis.*/
+
+	public iTween.EaseType movementCurve;
+	public iTween.LoopType animLoop;
+
+	private float m_velocity;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		iTween.MoveTo (gameObject, iTween.Hash ("x", xTargetPos, "easetype", movementCurve, "looptype", animLoop, "speed", moveSpeed));
+	}
+}
