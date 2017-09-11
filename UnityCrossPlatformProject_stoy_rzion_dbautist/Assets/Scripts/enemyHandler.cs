c18cae97b8f3304e26de106a4cf3ba25038d8e6a
Sebastian Toy <sebtoy@live.com>
2017-09-11 16:33:51 +1000

    - enemyHandler now moves the enemy ships in a side-side pattern as well as slowly moving down      - Standard transform movement, iTween has been abandoned due to complications

--- UnityCrossPlatformProject_stoy_rzion_dbautist/Assets/Scripts/enemyHandler.cs
+++ UnityCrossPlatformProject_stoy_rzion_dbautist/Assets/Scripts/enemyHandler.cs
@@ -6,19 +6,53 @@ public class enemyHandler : MonoBehaviour {
 
 	public float moveSpeed;			
 	public float xTargetPos;		/*Position to move to on the x axis.*/
+	public float yTargetPos;
 
-	public iTween.EaseType movementCurve;
-	public iTween.LoopType animLoop;
+	public iTween.EaseType horizontalMovementCurve;
+	public iTween.LoopType horizontalAnimLoop;
 
-	private float m_velocity;
+	public iTween.EaseType verticalMovementCurve;
+	public iTween.LoopType verticalAnimLoop;
+
+	private Vector3 m_velocity;
+
+    private bool m_moveRight = true;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        ///// NOTE: iTween can only move one piece of an object's transform at a time due to Unity setting the object's X, Y and Z all at once
+
+        //// Move down linearly
+        //iTween.MoveTo(gameObject, iTween.Hash("y", yTargetPos, "easetype", verticalMovementCurve, "looptype", verticalAnimLoop, "speed", moveSpeed));
+        
+        //// Loop moving right (and left once reached xTargetPos)
+        //iTween.MoveTo(gameObject, iTween.Hash("x", xTargetPos, "easeType", horizontalMovementCurve, "loopType", horizontalAnimLoop, "speed", moveSpeed));
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		iTween.MoveTo (gameObject, iTween.Hash ("x", xTargetPos, "easetype", movementCurve, "looptype", animLoop, "speed", moveSpeed));
-	}
+        /// Horizontal movement
+        // Reached right target position, signal to move left
+        if (transform.position.x >= xTargetPos) {
+            m_moveRight = false;
+        }
+        // Reached left target position, signal to move right
+        if (transform.position.x <= -xTargetPos) {
+            m_moveRight = true;
+        }
+
+        if (m_moveRight) {
+            m_velocity.x = moveSpeed * Time.deltaTime;
+        }
+        else {
+            m_velocity.x = -moveSpeed * Time.deltaTime;
+        }
+
+        /// Vertical movement
+        m_velocity.y = -(moveSpeed * 0.05f) * Time.deltaTime;       // Move down with percentile of regular force
+
+        // Update object position with calculated movement vector
+        transform.position += m_velocity;
+    }
 }
---
+++ UnityCrossPlatformProject_stoy_rzion_dbautist/Assets/Scripts/enemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemySpawner : MonoBehaviour {
+
+    public int      wave1Count;
+    public float    enemySpacing;
+
+	// Use this for initialization
+	void Start () {
+        /// First wave
+        float currentSpacing = enemySpacing / wave1Count; 
+
+        for (int i = 0; i < wave1Count; ++i) {
+            
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
