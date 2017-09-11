3208f887c54463be02e722c6ae18298005500ff3
ZetaFuntime <bautistade@gmail.com>
2017-09-11 15:16:53 +1000

    Bullet Spawn and Test

---
+++ UnityCrossPlatformProject_stoy_rzion_dbautist/Assets/Scripts/BulletTESTSTUFF/BulletSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour {
+
+    // Time interval for spawning
+    public float timeCount = 0f;
+    public float timeInterval = 2f;
+
+	public GameObject Bullet;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeCount += Time.deltaTime;
+
+		if (timeCount >= timeInterval) {
+			Instantiate (Bullet, this.transform.position, this.transform.rotation);
+			timeCount = 0f;
+		}
+	}
+}
